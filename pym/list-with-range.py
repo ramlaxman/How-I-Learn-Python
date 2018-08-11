ab6918d6f60c130d11e14da2abe9b26c3ed93516
Mayur Patil (मयूर पाटील) <ram.nath241089@gmail.com>
2018-08-11 15:33:41 +0530

    Create list-with-range.py

---
+++ pym/list-with-range.py
@@ -0,0 +1,14 @@
+# Full range
+range(10)
+list(range(10))
+
+#middle range
+range(5,10)
+list(range(5,10))
+
+# Ascending range with difference
+list(range(5,10,2))
+
+# Descending range with difference
+list(range(10,1,-1))
+
